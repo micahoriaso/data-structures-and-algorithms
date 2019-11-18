e6cc2863adb228dd5783df9903bfbd525b92af2a
Micah Oriaso <micahoriaso@gmail.com>
2019-11-18 18:08:38 +0300

    feat(list): add singly linked list remove by value

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -8,12 +8,27 @@ class Node {
 
 class LinkedList {
 
+    /*
+        The head, tail and size are properties of the linked list not the node chain.
+        The head and tail simply point to / are assigned a node in the node chain.
+        If the node chain is empty, the head and tail are assigned NULL because no node exists.
+    */
+
     constructor() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
+        
+    /*
+        case 1
+        head ===> null <==== tail
+        head ===> | 3 | <==== tail
 
+        case 2
+        head ===> | 3 | <==== tail
+        head ===> | 2 | ===> | 3 | <=== tail
+    */
     addFirst = node => {
 
         const tempNode = this.head;
@@ -26,13 +41,36 @@ class LinkedList {
 
     }
 
+
+    /*
+        case 1
+        head ===> null <==== tail
+        head ===> | 3 | <==== tail
+
+        case 2
+        head ===> | 3 | <==== tail
+        head ===> | 3 | ===> | 2 | <=== tail
+    */
     addLast = node => {
 
         this.size === 0 ? this.head = node : this.tail.next = node;
         this.tail = node;
+        this.size++;
 
     }
 
+    /*
+        case 1
+        head ===> null <==== tail
+
+        case 2
+        head ===> | 3 | <==== tail
+        head ===> null <==== tail
+
+        case 3
+        head ===> | 2 | ===> | 3 | <=== tail
+        head ===> | 3 | <=== tail
+    */
     removeFirst = () => {
 
         if (this.size === 0) return;
@@ -42,17 +80,82 @@ class LinkedList {
 
     }
 
+    /*
+        case 1
+        head ===> null <==== tail
+
+        case 2
+        head ===> | 3 | <==== tail
+        head ===> null <==== tail
+
+        case 3
+        head ===> | 2 | ===> | 3 | <=== tail
+        head ===> | 2 | <=== tail
+    */
     removeLast = () => {
         if (this.size === 0) return;
         if (this.size === 1) this.head = this.tail = null 
         else {
             let current = this.head;
-            while (current.next !== this.tail) {
+            while (current.next) {
                 current = current.next;
             }
 
             current.next = null;
             this.tail = current;
+        }
+        this.size --;
+
+    }
+
+    /*
+        case 1
+        head ===> null <==== tail
+
+        case 2
+        head ===> | 3 | <==== tail
+        head ===> null <==== tail
+
+        case 3
+        head ===> | 2 | ===> | 3 | <=== tail
+        head ===> | 2 | <=== tail
+
+        case 4
+        head ===> | 2 | ===> | 3 | ===> | 4 | <=== tail
+        head ===> | 3 | ===> | 4 | <=== tail
+
+        case 5
+        head ===> | 2 | ===> | 3 | ===> | 4 | <=== tail
+        head ===> | 2 | ===> | 4 | <=== tail
+    */
+    removeByValue = (node) => {
+
+        // case 1
+        if (this.size === 0) return;
+
+        if (this.size === 1) {
+            // case 2
+            if (this.head.value === node.value) this.head = this.tail = null;
+        } else {
+            let current = this.head;
+            let prevNode = null
+            while (current) {
+                if (current.value === node.value) {
+                    // case 3
+                    if (!prevNode)
+                    {
+                        this.head = current.next
+                    }
+                    else {
+                        // case 4
+                        prevNode.next = current.next;
+                    }
+
+                }
+                prevNode = current;
+                current = current.next;
+
+            }
         }
         this.size --;
 
@@ -79,13 +182,18 @@ class LinkedList {
 const node1 = new Node(10);
 const node2 = new Node(11);
 const node3 = new Node(12)
+const node4 = new Node(13)
 
 const myList = new LinkedList();
 
 myList.addFirst(node1);
 myList.addLast(node2);
 myList.addFirst(node3);
+myList.addLast(node4);
 
-myList.removeFirst();
+// myList.removeFirst();
 
-console.log(myList.enumarate());
+myList.removeByValue(node1);
+
+console.log('node list: ', myList.enumarate());
+console.log('size:', myList.size);
